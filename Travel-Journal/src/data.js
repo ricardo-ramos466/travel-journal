cca68159ca51f75429448d1892a137841ec4938b
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-10-14 22:14:16 -0400

    organizing data file

---
+++ Travel-Journal/src/data.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id: 1,
+        title: "Trip to Minnesota",
+        location: "Minnesota",
+        googleMapsUrl: "https://goo.gl/maps/mpv2D7ParnmjStCJ7",
+        startDate: "28 July, 2021",
+        endDate: "16 Dec, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+        tripPhotos:["minnesota-selfie.jpg", "minnesota-snow.jpg","minnesota-selfie.jpg","minnesota-dear.jpg"]
+    }
+]
